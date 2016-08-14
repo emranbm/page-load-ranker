a7c4581f61693bbe10cf07624efac45808e1f308
emranbm <emran.bm@gmail.com>
2016-08-14 10:43:11 +0430

    A bad mistake fixes. pushing to Array in php way! :|

--- controllers/rankerController.js
+++ controllers/rankerController.js
@@ -1,26 +1,37 @@
 /**
  * @param sites The array of URLs to check.
  **/
-module.exports = function (sites, f) {
+module.exports = function (sites) {
 
+    console.log('-1');
     var _ = require('lodash');
     var request = require('request');
 
+    console.log('0');
     var Promise = require('promise');
 
     var promises = [];
 
+    console.log('1');
+
     _.forEach(sites, (site) => {
-        promises[] = new Promise((resolve, reject) => {
+
+        console.log('2');
+        promises.push(new Promise((resolve, reject) => {
             request('http://' + site, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     resolve(site); // Show the HTML for the Google homepage.
                 }
             })
-        });
+        }));
     });
 
+
+    console.log('3');
+
     Promise.race(promises).then((site) => {
-        f('Rank #1 is ' + site);
+        console.log('Rank #1 is ' + site);
     });
-}
+
+    console.log('4');
+};
--- router.js
+++ router.js
@@ -2,12 +2,12 @@ module.exports = function (app) {
     app.get('/', (req, res) => {
         res.render('index');
     });
-    
-    app.post('/', (req,res) => {
-        if(req.body.sites){
+
+    app.post('/', (req, res) => {
+        if (req.body.sites) {
             var sites = req.body.sites.split(", ");
-            require('./controllers/rankerController.js')(sites, res);
-        }else{
+            require("./controllers/rankerController.js")(sites);
+        } else {
             res.redirect('/');
         }
     });
